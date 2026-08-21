c2e9e0bdf48b534d018eb9e36c0c7b231a683a93
maxieyy <upcloadtrial@gmail.com>
2026-08-22 02:16:41 +0300

    Fix SineWix series cache decorators

--- Public/SineWix/Routers/series.py
+++ Public/SineWix/Routers/series.py
@@ -5,7 +5,7 @@ from Core   import Request, kekik_cache, HTTPException
 from ..Libs import SineWixDB
 
 @sinewix_router.get("/serie/{series_id}")
-@kekik_cache(ttl=6 * 60 * 60, is_fastapi=True)
+@kekik_cache(ttl=6 * 60 * 60)
 async def get_serie(request: Request, series_id: str):
     try:
         sinewixdb = SineWixDB()
@@ -17,7 +17,7 @@ async def get_serie(request: Request, series_id: str):
         raise HTTPException(status_code=410, detail=f"{type(hata).__name__} » {hata}") from hata
 
 @sinewix_router.get("/series/{sayfa}")
-@kekik_cache(ttl=6 * 60 * 60, is_fastapi=True)
+@kekik_cache(ttl=6 * 60 * 60)
 async def get_series(request: Request, sayfa: str):
     try:
         sinewixdb = SineWixDB()
@@ -29,7 +29,7 @@ async def get_series(request: Request, sayfa: str):
         raise HTTPException(status_code=410, detail=f"{type(hata).__name__} » {hata}") from hata
 
 @sinewix_router.get("/series/{genre_id}/{sayfa}")
-@kekik_cache(ttl=6 * 60 * 60, is_fastapi=True)
+@kekik_cache(ttl=6 * 60 * 60)
 async def get_series_genre(request: Request, genre_id:str, sayfa: str):
     try:
         sinewixdb = SineWixDB()
