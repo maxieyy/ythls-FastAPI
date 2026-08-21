fe689f0237caac09d91c0256e16169162e9a8e9c
maxieyy <upcloadtrial@gmail.com>
2026-08-22 02:11:18 +0300

    Fix remaining is_fastapi cache decorators in OxAx cs3 router

--- Public/OxAx/Routers/cs3.py
+++ Public/OxAx/Routers/cs3.py
@@ -6,33 +6,33 @@ from ..Libs   import oxax
 from Settings import CACHE_TIME
 
 @oxax_router.get("/cs3/porno")
-@kekik_cache(ttl=CACHE_TIME, is_fastapi=True)
+@kekik_cache(ttl=CACHE_TIME)
 async def get_porno(request: Request):
     slugs = [slug for slug in await oxax.porno_kanal_listesi() if slug in oxax.detail]
 
     return {**oxax_global_message, "channels": [{"slug": slug, "detail": oxax.detail[slug]} for slug in slugs]}
 
 @oxax_router.get("/cs3/erotic")
-@kekik_cache(ttl=CACHE_TIME, is_fastapi=True)
+@kekik_cache(ttl=CACHE_TIME)
 async def get_erotic(request: Request):
     slugs = [slug for slug in await oxax.erotik_kanal_listesi() if slug in oxax.detail]
 
     return {**oxax_global_message, "channels": [{"slug": slug, "detail": oxax.detail[slug]} for slug in slugs]}
 
 @oxax_router.get("/cs3/hd")
-@kekik_cache(ttl=CACHE_TIME, is_fastapi=True)
+@kekik_cache(ttl=CACHE_TIME)
 async def get_hd(request: Request):
     slugs = [slug for slug in await oxax.hd_kanal_listesi() if slug in oxax.detail]
 
     return {**oxax_global_message, "channels": [{"slug": slug, "detail": oxax.detail[slug]} for slug in slugs]}
 
 @oxax_router.get("/cs3/detail/{slug}")
-@kekik_cache(ttl=CACHE_TIME, is_fastapi=True)
+@kekik_cache(ttl=CACHE_TIME)
 async def get_img(request: Request, slug:str):
     return {**oxax_global_message, **oxax.detail.get(slug)} if oxax.detail.get(slug) else {}
 
 @oxax_router.get("/cs3/search/{text}")
-@kekik_cache(ttl=CACHE_TIME, is_fastapi=True)
+@kekik_cache(ttl=CACHE_TIME)
 async def get_img(request: Request, text:str):
     return {
         **oxax_global_message,
